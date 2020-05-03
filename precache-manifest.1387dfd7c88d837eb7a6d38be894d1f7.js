eee27ebc74747a5434408e494d9075c530ade17c
Bohdan-lk <50949795+Bohdan-lk@users.noreply.github.com>
2020-05-03 13:42:29 +0300

    Updates

---
+++ precache-manifest.1387dfd7c88d837eb7a6d38be894d1f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75beeca3ac48c62068bb9e440ef65a0",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "bb360eb1101af7354abb",
+    "url": "/goit-react-hw-02-feedback/static/css/main.77d0ca0a.chunk.css"
+  },
+  {
+    "revision": "3a0d04128e1df7d69cd6",
+    "url": "/goit-react-hw-02-feedback/static/js/2.f780f501.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.f780f501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb360eb1101af7354abb",
+    "url": "/goit-react-hw-02-feedback/static/js/main.3706018f.chunk.js"
+  },
+  {
+    "revision": "f59aa7dbc0213e80a103",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb39d68.js"
+  }
+]);
