999f40a989dba3430f3b49d8c2641e8bbfdbbedc
Bohdan-lk <50949795+Bohdan-lk@users.noreply.github.com>
2020-05-03 13:41:34 +0300

    create  FedbackOptions, add git, gh-pages version 3

---
+++ src/components/Statistic/Statistic.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Styles from './Statistic.module.css';
+
+export default function Statistic({ label, value }) {
+	return (
+		<li className={Styles.item}>
+			<span className={Styles.label}>{label}</span>
+			{label !== 'Positive feedback' ? (
+				<div className={Styles.value}>{value}</div>
+			) : (
+				<div className={Styles.value}>{value} &#37;</div>
+			)}
+		</li>
+	);
+}
+Statistic.propTypes = {
+	label: PropTypes.string.isRequired,
+	value: PropTypes.number.isRequired,
+};
